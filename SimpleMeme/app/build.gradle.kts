521e94af850e503eb59eba11cd77882dca93c06e
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-05-10 07:43:43 +0300

    fix target sdk

--- SimpleMeme/app/build.gradle.kts
+++ SimpleMeme/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.company.simplememe"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.company.simplememe"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
