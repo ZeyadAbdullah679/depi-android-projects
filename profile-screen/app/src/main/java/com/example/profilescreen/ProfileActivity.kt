e13e9338d84b88d1fe3098661be8de2835f09129
Zeyad Abdullah <zeyadgalal93@gmail.com>
2024-07-17 14:06:53 +0300

    add callbacks to profile screen

--- profile-screen/app/src/main/java/com/example/profilescreen/ProfileActivity.kt
+++ profile-screen/app/src/main/java/com/example/profilescreen/ProfileActivity.kt
@@ -1,8 +1,13 @@
 package com.example.profilescreen
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.profilescreen.databinding.ActivityProfileBinding
@@ -23,5 +28,24 @@ class ProfileActivity : AppCompatActivity() {
         binding.toolbar.setNavigationOnClickListener {
             finish()
         }
+
+        binding.linkedin.onClickIconListener(LINKEDIN)
+        binding.facebook.onClickIconListener(FACEBOOK)
+        binding.github.onClickIconListener(GITHUB)
+
+    }
+
+    private fun ImageButton.onClickIconListener(url: String) {
+        setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+        }
+    }
+
+    companion object{
+        const val LINKEDIN = "https://www.linkedin.com/in/zeyadabdullah/"
+        const val FACEBOOK = "https://www.facebook.com/"
+        const val GITHUB = "https://github.com/ZeyadAbdullah679"
     }
 }
+
+
